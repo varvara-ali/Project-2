dce6e0184eb10f68f28c962d8d460b91474ce427
varvara-ali <varvara.alimova@gmail.com>
2024-12-05 14:46:51 +0300

    Переделала нафиг весь мэйн  Добавлена обработка успешности запросов и в целом теперь все хорошо

--- acuweather.py
+++ acuweather.py
@@ -1,10 +1,29 @@
 import requests
 import json
 from datetime import datetime
+from pprint import pprint
 
 api_key = "YAXJ2BgVH2LFs9nH0O9WZFYUjfkV4nXm"
 
-# Полученим ключ для определения кода города по координатам
+
+# Оценка погодных условий
+def weather_quality(weather):
+    bad_conditions = []
+    if weather['temperature'] < 0:
+        bad_conditions.append(f"Температура слишком низкая: {weather['temperature']}")
+    if weather['temperature'] > 35:
+        bad_conditions.append(f"Температура слишком большая: {weather['temperature']}")
+    if weather['wind_speed'] >= 40:
+        bad_conditions.append(f"Скорость ветра слишком большая: {weather['wind_speed']} метров в секунду")
+    if weather['precipitation_probability'] > 70:
+        bad_conditions.append(f"Вероятность выпадения осадков слишком высока: {weather['precipitation_probability']}%")
+    if weather['relative_humidity'] > 80:
+        bad_conditions.append(f"Слишком большая влажность: {weather['relative_humidity']}%")
+
+    return bad_conditions
+
+
+# Полученим ключ для определения кода города по координатам доп функция для получения погоды
 def lock_key_by_cords(latitude, longitude):
     location_url = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search"
     params = {
@@ -25,62 +44,53 @@ def lock_key_by_cords(latitude, longitude):
     return {
             'success': True,
             'key': location_key
+
         }
 # наверно есть какой-то смысл почему у них код города, а не название  Хотя повторы..
 
-# Получение погоды по ключу, который получили выще
-def get_weather(location_key):
-    params = {
-        "apikey": api_key,
-        "details": True,
-        'metric': True
-    }
-    url = f'http://dataservice.accuweather.com/forecasts/v1/daily/1day/{location_key}'
-    response = requests.get(url, params=params)
-    return response.json()
-
+# Получение погоды по координатам, используя функцию выше, который получили выще
+def get_weather(latitude, longitude):
+    location_key = lock_key_by_cords(latitude,longitude)
+    print(location_key)
+    try:
+        params = {
+            "apikey": api_key,
+            "details": True,
+            'metric': True
+        }
+        url = f'http://dataservice.accuweather.com/forecasts/v1/daily/1day/{location_key['key']}'
+        response = requests.get(url, params=params).json()
+        weather_json = {
+            'cord': f"{latitude}; {longitude}",
+            'date': datetime.now().strftime("%d-%m-%Y"),
+            'temperature': int(
+                (response['DailyForecasts'][0]['Temperature']['Minimum']['Value'] +
+                 response['DailyForecasts'][0]['Temperature']['Maximum']['Value']) / 2),
+            'relative_humidity': int(response['DailyForecasts'][0]['Day']['RelativeHumidity']['Average']),
+            'precipitation_probability': int(response['DailyForecasts'][0]['Day']['PrecipitationProbability']),
+            'wind_speed': int(response['DailyForecasts'][0]['Day']['Wind']['Speed']['Value'])}
+        weather_json['bad_condition'] = weather_quality(weather_json)
+        # Доп проверка на успешность
+        return {'success': True,
+                'weather': weather_json
+                }
+    except Exception as e:
+        return {'success': False,
+                'error': f"Проблемы с доступом к api: \n{str(e)}"
+                }
 
 def main():
     # Возьму в качестве примера Тулу
     latitude = 54.1961
     longitude = 37.6182
-    result = lock_key_by_cords(latitude, longitude)
-
-    if result['success']:
-        location_key = result['key']
-        weather = get_weather(location_key)
-        weather_json = {
-            'cord': f"{latitude}; {longitude}",
-            'date': datetime.now().strftime("%d-%m-%Y"),
-            'temperature': int(
-                (weather['DailyForecasts'][0]['Temperature']['Minimum']['Value'] +
-                 weather['DailyForecasts'][0]['Temperature']['Maximum']['Value'])/2),
-            'relative_humidity': int(weather['DailyForecasts'][0]['Day']['RelativeHumidity']['Average']),
-            'precipitation_probability': int(weather['DailyForecasts'][0]['Day']['PrecipitationProbability']),
-            'wind_speed': int(weather['DailyForecasts'][0]['Day']['Wind']['Speed']['Value'])}
-        print(weather_json)
-        with open('accu.json', 'w') as f:
-            json.dump(weather_json, f, indent=4)
+    weather_result = get_weather(latitude, longitude)
+    if weather_result['success']:
+        weather = weather_result['weather']
+        print(weather)
+        with open('accu.json', 'w', encoding='utf-8') as f:
+            json.dump(weather, f, indent=4)
     else:
-        print(result['error'])
-
-
-# Оценка погодных условий
-def weather_quality(weather):
-    bad_conditions = []
-    if weather['temperature'] < 0:
-        bad_conditions.append(f"Температура слишком низкая: {weather['temperature']}")
-    if weather['temperature'] > 35:
-        bad_conditions.append(f"Температура слишком большая: {weather['temperature']}")
-    if weather['wind_speed'] >= 40:
-        bad_conditions.append(f"Скорость ветра слишком большая: {weather['wind_speed']} метров в секунду")
-    if weather['precipitation_probability'] > 70:
-        bad_conditions.append(f"Вероятность выпадения осадков слишком высока: {weather['precipitation_probability']}%")
-    if weather['relative_humidity'] > 80:
-        bad_conditions.append(f"Слишком большая влажность: {weather['relative_humidity']}%")
-
-    return bad_conditions
-
+        print(weather_result['error'])
 
 if __name__ == '__main__':
     main()
