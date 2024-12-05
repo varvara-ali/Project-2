f3b668d894e50cf4a049a25464ccf2252b80d71c
varvara-ali <varvara.alimova@gmail.com>
2024-12-05 16:58:17 +0300

    Weather by name is done

--- acuweather.py
+++ acuweather.py
@@ -3,8 +3,10 @@ import json
 from datetime import datetime
 from pprint import pprint
 
+from geopy.geocoders import Nominatim
+
 # api_key = "YAXJ2BgVH2LFs9nH0O9WZFYUjfkV4nXm"
-api_key = "NRsCPXZ9aYVBjl28TqxjvfWZ1xhA8Occ"
+api_key = "ictE4TEiYBIjKOkZsZyF3GG8dcGeDI3U"
 
 # Оценка погодных условий
 def weather_quality(weather):
@@ -49,19 +51,17 @@ def lock_key_by_cords(latitude, longitude):
 # наверно есть какой-то смысл почему у них код города, а не название  Хотя повторы..
 
 # Получение погоды по координатам, используя функцию выше, который получили выще
-def get_weather(latitude, longitude):
-    location_key = lock_key_by_cords(latitude,longitude)
-    print(location_key)
+def get_weather(location_key, name):
     try:
         params = {
             "apikey": api_key,
             "details": True,
             'metric': True
         }
-        url = f'http://dataservice.accuweather.com/forecasts/v1/daily/1day/{location_key['key']}'
+        url = f'http://dataservice.accuweather.com/forecasts/v1/daily/1day/{location_key}'
         response = requests.get(url, params=params).json()
         weather_json = {
-            'cord': f"{latitude}; {longitude}",
+            'name': name,
             'date': datetime.now().strftime("%d-%m-%Y"),
             'temperature': int(
                 (response['DailyForecasts'][0]['Temperature']['Minimum']['Value'] +
@@ -79,18 +79,48 @@ def get_weather(latitude, longitude):
                 'error': f"Проблемы с доступом к api: \n{str(e)}"
                 }
 
+def get_cords(city_name):
+    try:
+        # params = {
+        #     "apikey": api_key,
+        #     "details": True,
+        #     'metric': True
+        # }
+        url = f'http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={city_name}&language=en-us&details=true'
+        response = requests.get(url).json()
+        return response[0]['Key']
+    except Exception as e:
+        return {'success': False,
+                'error': f"Проблемы с доступом к api: \n{str(e)}"
+                }
+
+
+
 def main():
     # Возьму в качестве примера Тулу
     latitude = 54.1961
     longitude = 37.6182
-    weather_result = get_weather(latitude, longitude)
+    # loc_key = lock_key_by_cords(latitude, longitude)
+    # weather_result = get_weather(loc_key['key'], f"{latitude}; {longitude}")
+    # if weather_result['success']:
+    #     weather = weather_result['weather']
+    #     print(weather)
+    #     with open('accu.json', 'w', encoding='utf-8') as f:
+    #         json.dump(weather, f, indent=4)
+    # else:
+    #     print(weather_result['error'])
+
+    name = "Moscow"
+    city_key = get_cords(name)
+    weather_result = get_weather(city_key, name)
     if weather_result['success']:
         weather = weather_result['weather']
-        print(weather)
+        pprint(weather)
         with open('accu.json', 'w', encoding='utf-8') as f:
             json.dump(weather, f, indent=4)
     else:
         print(weather_result['error'])
+
 
 if __name__ == '__main__':
     main()
--- site.py
+++ site.py
@@ -1,3 +1,5 @@
+from os import access
+
 from flask import Flask, request, render_template, jsonify
 import acuweather
 
@@ -21,7 +23,9 @@ def weather_by_cords():
         latitude = form['latitude']
         longitude = form['longitude']
         print(latitude, longitude)
-        data = acuweather.get_weather(latitude, longitude)
+        cords_key = acuweather.lock_key_by_cords(latitude, longitude)
+        print(cords_key)
+        data = acuweather.get_weather(cords_key['key'], f"{latitude}; {longitude}")
         print(data)
         if data['success']:
             weather = data['weather']
@@ -31,6 +35,32 @@ def weather_by_cords():
             error = data['error']
             print(error)
             return render_template('weather_by_cords.html', error=error)
+
+
+# получение погоды в городе
+@site.route('/weather_by_city', methods=['GET', 'POST'])
+def weather_by_city():
+    if request.method == 'GET':
+        return render_template('weather_by_city.html')
+    elif request.method == 'POST':
+        data = request.form
+        city_name = data['city_name']
+        city_key = acuweather.get_cords(city_name)
+
+
+
+        if city_key:
+            data = acuweather.get_weather(city_key, city_name)
+            if data['success']:
+                weather = data['weather']
+                return render_template('weather_in_point.html', weather=weather)
+            else:
+                error = data['error']
+                return render_template('weather_by_city.html', error=error)
+
+        else:
+            return render_template('weather_by_city.html', error='Указанный город не найден')
+
 
 if __name__ == '__main__':
     site.run(port=8000, debug=True)
